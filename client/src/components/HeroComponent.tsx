1f7add6f6daff9d468f276ef7c0a49be4da519ea
Dan <danmil511@gmail.com>
2025-06-12 21:01:01 -0500

    Added SearchBar component to hero section

--- client/src/components/HeroComponent.tsx
+++ client/src/components/HeroComponent.tsx
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import SearchBar from "./SearchBar";
 
 const HeroComponent = () => {
   return (
@@ -11,24 +12,22 @@ const HeroComponent = () => {
           alt="HeroImage"
         />
       </div>
-      <div className="relative z-10 flex flex-col items-center h-full justify-between mt-20 gap-20 mx-10 md:items-start md:mx-20 md:my-30">
+      <div className="relative z-10 flex flex-col items-center h-full justify-between mt-20 gap-15 mx-10 md:items-center md:mx-20 md:my-30">
         <h1 className="bg-white/70 rounded-3xl p-3 text-sm md:text-lg">
           The world is wide. Go see it.
         </h1>
         <div className="text-center">
-          <h1 className="text-5xl font-bold bg-gradient-to-br from-white to-white bg-clip-text text-transparent text-center drop-shadow-2xl md:text-7xl md:text-start py-1">
-            Plan your next journey with ease..
+          <h1 className="text-1xl font-bold bg-gradient-to-r from-white/60 to-white/70 bg-clip-text text-transparent text-center drop-shadow-2xl shadow-black md:text-3xl">
+            YOUR NEXT
           </h1>
-          <h1 className="text-4xl font-bold bg-gradient-to-r from-white to-white/60 bg-clip-text text-transparent text-center mt-10 drop-shadow-2xl md:text-start md:text-6xl">
-            inspiration,
+          <h1 className="text-5xl font-bold bg-gradient-to-r from-white to-white/60 bg-clip-text text-transparent text-center drop-shadow-2xl md:text-[8rem]">
+            ADVENTURE
           </h1>
-          <h1 className="text-3xl font-bold bg-gradient-to-r from-white/60 to-white/70 bg-clip-text text-transparent text-center mt-2 drop-shadow-2xl shadow-black md:text-start md:text-5xl">
-            and zero stress.
+          <h1 className="text-1xl font-bold bg-gradient-to-r from-white/60 to-white/70 bg-clip-text text-transparent text-center mt-5 drop-shadow-2xl shadow-black md:text-3xl">
+            Plan your next journey with ease, inspiration, and zero stress.
           </h1>
         </div>
-        <Button className="bg-white rounded-3xl p-4 font-semibold cursor-pointer hover:bg-white/90">
-          Explore now
-        </Button>
+        <SearchBar />
       </div>
     </div>
   );
---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+import { Search } from "lucide-react";
+
+const SearchBar = () => {
+  return (
+    <div className="bg-white rounded-4xl w-full mx-auto font-semibold drop-shadow-xl">
+      <form
+        className="flex items-center p-1 md:flex-row gap-4 md:justify-start w-full"
+        action=""
+      >
+        <div className="flex md:flex-row gap-4 flex-1 p-1">
+          <div className="hidden md:flex justify-center items-center bg-blue-100 rounded-4xl p-4">
+            <img src="/src/assets/pin.png" alt="" width={150} />
+          </div>
+          <div className="flex flex-col w-full p-3">
+            <h1 className="hidden md:block mb-1">Location</h1>
+            <input
+              type="text"
+              className="text-sm rounded w-full"
+              placeholder="Search your destination"
+            />
+          </div>
+          <div className="hidden md:flex justify-center items-center bg-violet-100 rounded-4xl p-4">
+            <img src="/src/assets/date.png" alt="" width={150} />
+          </div>
+          <div className="hidden md:flex flex-col w-full p-3">
+            <h1 className="mb-1">Date</h1>
+            <input
+              type="text"
+              className="text-sm rounded w-full"
+              placeholder="Choose a date"
+            />
+          </div>
+          <div className="hidden md:flex justify-center items-center bg-green-100 rounded-4xl p-4">
+            <img src="/src/assets/dollar.png" width={150} />
+          </div>
+
+          <div className="hidden md:flex flex-col w-full p-3">
+            <h1 className="mb-1">Price</h1>
+            <input
+              type="text"
+              className="text-sm rounded w-full"
+              placeholder="Your budget"
+            />
+          </div>
+        </div>
+        <button className="bg-blue-500 rounded-4xl p-1 flex items-center justify-center gap-1 cursor-pointer md:p-3">
+          <h1 className="hidden md:block rounded-4xl text-white font-normal mx-2">
+            Find my adventure
+          </h1>
+          <div className="md:bg-white rounded-4xl p-2.5 text-white md:text-black">
+            <Search />
+          </div>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
