c5be3e0e50b2884b852a16494d945adb92488c33
Dan <danmil511@gmail.com>
2025-06-20 14:37:12 -0500

    Changed controllers to get data from database

--- server/controllers/attraction.controller.ts
+++ server/controllers/attraction.controller.ts
@@ -2,6 +2,7 @@ import User from "../models/user.model";
 import Attraction from "../models/attraction.model";
 import { Request, Response } from "express";
 import axios from "axios";
+import City from "../models/city.model";
 
 interface City {
   name: string;
@@ -86,86 +87,54 @@ export const attractions = async (
   }
 
   try {
-    const geocodeResponse = await axios.get(
-      "https://api.geoapify.com/v1/geocode/search",
-      {
-        params: { text: destination, apiKey, limit: 1 },
-      }
-    );
-
-    if (!geocodeResponse.data.features.length) {
-      res.status(404).json({ error: "City not found" });
+    const city = await City.findOne({ name: destination });
+    if (!city) {
+      res.status(404).json({ message: "City not found" });
       return;
     }
 
-    const { lat, lon } = geocodeResponse.data.features[0].properties;
+    const attractions = await Attraction.find({ city: city._id });
 
-    const attractionsResponse = await axios.get(
-      "https://api.geoapify.com/v2/places",
-      {
-        params: {
-          categories: "tourism",
-          filter: `circle:${lon},${lat},3000`,
-          fields:
-            "place_id,name,wiki_and_media,website,state,lon,lat,formatted,opening_hours,categories",
-          limit: 20,
-          apiKey,
-        },
-      }
-    );
-
-    if (!attractionsResponse.data.features) {
-      res.json([]);
-    }
-    const formattedAttractions = await Promise.all(
-      attractionsResponse.data.features
-        .filter(
-          (att: any) =>
-            (att.properties.name_international?.en || att.properties.name) &&
-            att.properties.wiki_and_media?.wikimedia_commons
-        )
-        .map(async (att: any) => {
-          const wikiCommons = att.properties.wiki_and_media?.wikimedia_commons;
-          const image = wikiCommons
-            ? await fetchWikimediaImage(wikiCommons)
-            : null;
-
-          return {
-            placeId: att.properties.place_id,
-            name: att.properties.name_international?.en || att.properties.name,
-            image,
-            website: att.properties.website,
-            state: att.properties.state,
-            lon: att.properties.lon,
-            lat: att.properties.lat,
-            address: att.properties.formatted,
-            opening_hours: att.properties.opening_hours,
-            categories: att.properties.categories,
-          };
-        })
-    );
-
-    res.json(formattedAttractions);
+    res.json(attractions);
   } catch (error: any) {
     console.error("Error fetching attractions:", error.message);
     res.status(500).json({ message: "Server Error" });
   }
 };
 
-export const favoriteAttraction = async (req: Request, res: Response) => {
+export const favoriteAttraction = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { favAttraction } = req.body;
     const userId = req.user._id;
-    let attraction = await Attraction.findOne({
-      placeId: favAttraction.placeId,
-    });
+    const attraction = await Attraction.findById(favAttraction._id);
     if (!attraction) {
-      attraction = await Attraction.create(favAttraction);
+      res
+        .status(404)
+        .json({ error: `Attraction with ID ${favAttraction._id} not found` });
+      return;
     }
-    await User.findByIdAndUpdate(userId, {
-      $addToSet: { favoriteAttractions: attraction._id },
+    // Update user's favorite attractions
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      { $addToSet: { favoriteAttractions: attraction._id } },
+      { new: true, runValidators: true }
+    ).populate("favoriteAttractions", "name cityName"); // Populate attraction details
+
+    if (!updatedUser) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+    res.status(201).json({
+      message: "Attraction added to favorite attractions successfully",
+      attraction: {
+        _id: attraction._id,
+        name: attraction.name,
+      },
+      favoriteAttractions: updatedUser.favoriteAttractions,
     });
-    res.status(200).json(attraction);
   } catch (error: any) {
     console.error("Error fetching favorite attractions:", error.message);
     res.status(500).json({ message: "Server Error" });
@@ -174,16 +143,18 @@ export const favoriteAttraction = async (req: Request, res: Response) => {
 
 export const getCities = async (req: Request, res: Response) => {
   const { city } = req.query;
+  if (!city || typeof city !== "string") {
+    return;
+  }
+  const upperCity = city
+    ?.toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
   try {
-    const response = await axios.get("https://api.api-ninjas.com/v1/city", {
-      params: {
-        name: city,
-      },
-      headers: {
-        "X-Api-Key": process.env.NINJA_API_KEY,
-      },
+    const cities = await City.find({
+      name: { $regex: `^${upperCity}`, $options: "i" }, // find cities that starts with
     });
-    const cities = response.data;
     if (!cities) {
       res.status(404).json({ message: "Invalid city" });
     }
